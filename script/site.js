f96010200b4cc4d0f610c51c2aaee453fa1350c8
Fox <800057489@fvtc.edu>
2025-02-04 12:06:13 -0600

    Added welcome div

---
+++ script/site.js
@@ -0,0 +1,27 @@
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const welcome = document.querySelector('#welcome')
+const message = document.createElement('h1')
+
+
+welcome.innerHTML = ''
+// message.textContent = 'Testing'
+// welcome.append(message)
+
+if (hour = isMorning) {
+    message.textContent = 'Good Morning'
+    welcome.append(message)
+}
+
+else if (hour = isAfternoon) {
+     message.textContent = 'Good Afternoon'
+     welcome.append(message)
+}
+else {
+     message.textContent = 'Good Evening'
+     welcome.append(message)
+}
